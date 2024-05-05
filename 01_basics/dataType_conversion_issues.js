057315adf3b947c9a1562020dbec1fc69e558608
waseem akram <mw5667155@gmail.com>
2024-05-06 01:39:15 +0500

    dataType_conversion file is added

---
+++ 01_basics/dataType_conversion_issues.js
@@ -0,0 +1,28 @@
+let pName = "waseem akram";
+let score = "33";
+
+//console.log(pName);
+//console.log(typeof pName); //ofc string
+let totalScore = Number(score);
+let numpName = Number(pName);
+//console.log(numpName); //this will give us NaN which is not a number
+//console.log(typeof totalScore); // it will give us 33 which is number
+// 33=> 33
+
+let randomNumber = 45;
+//console.log(typeof randomNumber);
+let totalRandomNumber = String(randomNumber);
+//console.log(typeof totalRandomNumber); //it will give 45 but in string
+
+let isMarried = true;
+let checkIt = Number(isMarried);
+//console.log(checkIt); //give me 1 which is integar
+// console.log(parseInt(pName)); //it will give me NaN..
+//let check = "hello"; // true
+let check = ""; // false
+
+let checkBolean = Boolean(check);
+console.log(checkBolean); //it will give me true
+
+// 1 => true , 0 => false
+// "hello" => true ,"" => false
