6987c32328457ebc7a1ba3cd4f89d2762f787c89
waseem akram <mw5667155@gmail.com>
2024-05-15 23:09:03 +0500

    Number and Math library done

---
+++ 01_basics/Number_Math.js
@@ -0,0 +1,35 @@
+const score = 400;
+//console.log(score);
+const gamescore = new Number(100);
+//console.log(gamescore); // Object  and output will be like [100]
+//console.log(gamescore.toExponential()); //In the form of e
+//console.log(gamescore.toFixed(2)); //100.00
+//console.log(gamescore.toLocaleString()); // converts it into string | 10,000,000
+//console.log(gamescore.toPrecision(2));
+//console.log(gamescore.toString()); // it will convert the number into string
+//console.log(gamescore.valueOf()); //
+//console.log(Number.EPSILON);
+//console.log(Number.MAX_SAFE_INTEGER.toExponential()); //The maximum safe integer in JavaScript (253 - 1).
+//console.log(Number.MIN_SAFE_INTEGER.toExponential()); // The minimum safe integer in JavaScript (-253 - 1).
+const biggestNum = Number.MAX_VALUE;
+const smallestNum = Number.MIN_VALUE;
+const infiniteNum = Number.POSITIVE_INFINITY;
+const negInfiniteNum = Number.NEGATIVE_INFINITY;
+const notANum = Number.NaN;
+
+//++++++++++++++++++++++++ MATH ++++++++++++++++++++++++++++++++++
+//console.log(Math); // object Math
+
+//console.log(Math.random()); //it will give random values between 0 and 1 ;
+//console.log(Math.random() * 10 + 1);
+//console.log(Math.floor(Math.random() * 10 + 1));
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max - min) + 1 + min));
+// Math.ceil
+// Math.round
+// Math.max
+// Math.min
+// Math.sqrt
+//
---
+++ 01_basics/string.js
@@ -0,0 +1,25 @@
+const name = "waseem akram";
+const snapscore = "150000";
+console.log(`${name} has ${snapscore}`);
+
+const gameName = new String("pubg");
+console.log(gameName.__proto__); //it will give us the list of all predefined functions .
+// console.log(typeof gameName); // it is object
+/*
+{ 0: 'p',
+  1: 'u',
+  2: 'b',
+  3: 'g',
+}
+*/
+//console.log(gameName[2]); //b;
+//console.log(gameName.toUpperCase()); //PUBG
+//console.log(gameName.anchor("pubg")); //<a name="pubg">pubg</a>
+console.log(gameName.charAt(3)); //g
+//console.log(gameName.blink());
+//console.log(gameName.fontsize()); //it give us <font size="undefined">pubg</font>
+console.log(gameName.slice(-3, 4)); //end wale 3 dega like in pubg it will give ubg.
+console.log(gameName.substring(-2, 4)); //it ignore negative values..it start from 0
+// console.log(2 + "2");
+let sum = "2" + 2;
+console.log(typeof sum);
