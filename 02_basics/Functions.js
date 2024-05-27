79ca418f51740d1dbcbee68cdfb4f9f1c3d54b0b
waseem akram <mw5667155@gmail.com>
2024-05-27 22:43:44 +0500

    add some more thing

--- 02_basics/Functions.js
+++ 02_basics/Functions.js
@@ -1,27 +1,40 @@
 //console.log("hello world");
 
-const userName=function(){
-    console.log("hello waseem_akram");
+const userName = function () {
+  console.log("hello waseem_akram");
+};
+//userName() // function call.
+
+const sum = function (a, b) {
+  return a + b;
+};
+// const total=sum(5,"5"); //55
+//const total=sum(5,null); //5
+//const total=sum(5,undefined); //NAN
+//const total=sum(5,Symbol); //5 function symbol()
+//console.log(total);
+
+const userLoggedIn = function (name) {
+  if (!name) {
+    return;
   }
-  //userName() // function call.
-  
-  
-  const sum=function(a,b){
-    return a+b;
-  }
-  // const total=sum(5,"5"); //55
-  //const total=sum(5,null); //5
-  //const total=sum(5,undefined); //NAN
-  //const total=sum(5,Symbol); //5 function symbol()
-  //console.log(total);
-  
-  const userLoggedIn=function(name){
-    if(!name){
-      return 
-    }
-    console.log(`hello user ${name} is loggedin`)
-  }
-  //console.log(userLoggedIn("waseem"));
-  //console.log(userLoggedIn(""));
-  //console.log(userLoggedIn(undefined));
-  //console.log(userLoggedIn(null));
+  console.log(`hello user ${name} is loggedin`);
+};
+//console.log(userLoggedIn("waseem"));
+//console.log(userLoggedIn(""));
+//console.log(userLoggedIn(undefined));
+//console.log(userLoggedIn(null));
+
+function myFunc(theObject) {
+  theObject.make = "Toyota";
+}
+
+const mycar = {
+  make: "Honda",
+  model: "Accord",
+  year: 1998,
+};
+
+console.log(mycar.make); // "Honda"
+myFunc(mycar);
+console.log(mycar.make); // "Toyota"
