3b5c0f3d3e8cb1de6b0ec9632af746f705ff20a4
waseem akram <mw5667155@gmail.com>
2024-05-27 22:38:35 +0500

    Functions part One done

---
+++ 02_basics/Functions.js
@@ -0,0 +1,27 @@
+//console.log("hello world");
+
+const userName=function(){
+    console.log("hello waseem_akram");
+  }
+  //userName() // function call.
+  
+  
+  const sum=function(a,b){
+    return a+b;
+  }
+  // const total=sum(5,"5"); //55
+  //const total=sum(5,null); //5
+  //const total=sum(5,undefined); //NAN
+  //const total=sum(5,Symbol); //5 function symbol()
+  //console.log(total);
+  
+  const userLoggedIn=function(name){
+    if(!name){
+      return 
+    }
+    console.log(`hello user ${name} is loggedin`)
+  }
+  //console.log(userLoggedIn("waseem"));
+  //console.log(userLoggedIn(""));
+  //console.log(userLoggedIn(undefined));
+  //console.log(userLoggedIn(null));
