ec25a97023f6975101a0b7c081013d4283eeabd0
waseem akram <mw5667155@gmail.com>
2024-05-27 22:14:31 +0500

    object_destructring done

---
+++ 02_basics/objectdestructring.js
@@ -0,0 +1,15 @@
+//console.log("hello world ");
+
+const course={
+    course_name:"Ultimate js course ",
+      course_id:999,
+      course_instructor:"waseem_akram",
+    }
+    const{course_instructor:teacher}=course;
+    //console.log(course_instructor);
+    //console.log(teacher)
+    
+    
+    //json api best
+    
+    //https://randomuser.me/api/
