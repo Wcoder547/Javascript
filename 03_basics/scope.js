9d93a1e540750408e3218c3d753d64d88a14c0e7
wcoder547 <mw5667155@gmail.com>
2024-06-12 09:58:49 +0500

    funjs and scope variable is done

---
+++ 03_basics/scope.js
@@ -0,0 +1,50 @@
+// console.log("hello world");
+let a = 10;
+const b = 20;
+var c = 30;
+if (true) {
+  let a = 40;
+  const b = 50;
+  //console.log("inner", c); //inner 30
+  var c = 60;
+}
+// console.log(a); //10
+// console.log(b); //20
+// console.log(c); //60
+let x = "hello this is me .hacker";
+const func1 = function () {
+  let x = "hello this is x";
+
+  console.log(x);
+};
+//func1();
+//console.log(x);
+const func2 = function () {
+  const y = "hello this is y";
+  console.log(y);
+};
+//func2();
+//console.log(y);// y is not defined ...
+var z = "hello this is wwwz";
+const func3 = function () {
+  var z = "hello this is z";
+  console.log(z);
+};
+
+//func3();
+//console.log(z);
+
+{
+  var a1 = "wasim";
+}
+console.log(a1); //wasim
+
+{
+  const a2 = "qasim";
+}
+//console.log(a2); // not a2 defined
+
+{
+  let a4 = "ali";
+}
+//console.log(a4); // a4 is not defined
---
+++ Fun_js/fun1.js
@@ -0,0 +1,20 @@
+let a = 2;
+let b = 3;
+const waseem = function (a, b) {
+  a = 5;
+  b = 10;
+  console.log(a + b);
+};
+waseem(a, b);
+//console.log("waseem akram");
+const arr1 = [
+  "arr1",
+  {
+    name: "waseem akram",
+    class: "bsit",
+  },
+];
+console.log(arr1);
+console.log(typeof arr1); //it's object..
+const c = arr1[1];
+console.log(c);
